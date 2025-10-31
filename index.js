49bb84a16b511777f2a4d29c76f23c03292d9d9a
su469843 <128950064+su469843@users.noreply.github.com>
2025-10-31 12:28:26 +0000

    dsd

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { LogBox, AppRegistry } from 'react-native';
+
+// 忽略所有日志通知（这步可选，但能减少干扰）
+LogBox.ignoreAllLogs();
+
 import App from './App';
 import { name as appName } from './app.json';
 
